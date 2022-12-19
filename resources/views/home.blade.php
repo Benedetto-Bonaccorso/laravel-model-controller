03104154f55dd0b9abc8d655b498496736ea8949
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-12-19 16:57:31 +0100

    consegna

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/js/app.js')
 </head>
 <body>
 
-    <h1>{{$movies}}</h1>
+    <div class="container">
+        <div class="row">
+            @foreach($movies as $movie)
+                <div class="col-3 p-4">
+                    <div class="card movie text-center py-5">
+                        <h1 class="py-3">{{$movie['title']}}</h1>
+                        <p class="py-3">{{$movie['date']}}</p>
+                        <p class="py-3">{{$movie['vote']}}</p>
+                    </div>
+                </div>   
+            @endforeach
+        </div>
+    </div>
+
 
 </body>
 </html>
